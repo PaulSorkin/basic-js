c32b1aac66b682ff149a3aec5dbe0d3859c998f4
Paul <sorkinpavel@mail.ru>
2022-05-02 19:25:23 +0500

    some solved

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,11 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let myArr = Array.from(n.toString());
+  resArr = myArr.map(el => +el).sort((a, b) => b - a);
+  let result = '';
+  for (let i=0; i < myArr.length - 1; i++) {
+    result = result + resArr[i];
+  }
+  return +result;
 }
 
 module.exports = {
   deleteDigit
 };
+
--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,11 +10,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
+function getEmailDomain(email) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  function redDog() {
+    if (email.includes('@')) {
+      email = email.slice(email.indexOf('@') + 1);
+      redDog();
+    }
+    return email;
+  }
+  redDog();
+  return email;
 }
 
 module.exports = {
   getEmailDomain
 };
+
+getEmailDomain('very.unusual.@.unusual.com@usual.com')
